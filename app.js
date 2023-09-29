5787c70026ae445d325d8875de8c932e4b1efdd4
Shreyash Daundkar <productiveappa@gmail.com>
2023-09-29 08:49:00 +0530

    done

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const http = require('http');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('first middelware');
-    next();
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.post('/add-product', (req, res, next) => {
+    res.send('<form action="/products" method="POST"><input type="text" name="name"><input type="number" name="price"><input type="submit" value="Add Product"></form>');
 });
 
-app.use((req, res, next) => {
-    console.log('second middelware');
-    res.send({ "key1" : 4 });
+app.post('/products', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Welcome</h1><form action="/add-product" method="POST"><input type="submit" value="Go To Add Product"></form>');
 });
 
 app.listen(4000);
