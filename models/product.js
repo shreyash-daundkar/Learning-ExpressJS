0be8bd0f8a5cda9058a5202c22d93bb343820d82
Shreyash Daundkar <productiveappa@gmail.com>
2023-10-04 15:58:23 +0530

    changed code in fetch, edit, delete functionality in product model to work with mysql databse  instead of file

--- models/product.js
+++ models/product.js
@@ -2,6 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const rootDir = require('../util/path');
 const cartModel = require('./cart');
+const db = require('../util/database');
 
 const dataPath = path.join(rootDir, 'data', 'products.json');
 
@@ -10,57 +11,29 @@ module.exports = class Product {
         this.name = p.name;
         this.price = p.price;
     }
-
-    async save() {
-        const data = await Product.fetchAll();
-        this.id = data.length == 0 ? 1 : data[data.length - 1].id + 1;
-        data.push(this);
-        fs.writeFile(dataPath, JSON.stringify(data) , err => {
-            if(err) throw err;
-        });
+    
+    save() {
+        db.execute('INSERT INTO products (name, price) VALUES (?, ?)', [this.name, this.price]);
     }
 
-    static fetchAll() {
-        return new Promise((resolve, reject) => {
-            fs.readFile(dataPath, (err, data) => {
-                if(err) reject();
-                try {
-                    resolve(JSON.parse(data));
-                } catch(e) {
-                    resolve([]);
-                }
-            });
-        });
+    static async fetchAll() {
+        const [row, meta] = await db.execute('SELECT * FROM products');
+        return row;
     }
 
     static async productDetails(id) {
-        const products = await Product.fetchAll();
-        let res = null;
-        products.forEach(product => {
-            if(product.id == parseInt(id)) res = product;
-        });
-        return res;
+        const [row, meta] = await db.execute(`SELECT * FROM products WHERE id = ${id} `);
+        return row[0];
     }
 
     static async edit(id, data) {
-        const products = await Product.fetchAll();
-        products.forEach(product => {
-            if(product.id == parseInt(id)) {
-                product.name = data.name;
-                product.price = data.price;
-            }
-        });
-        fs.writeFile(dataPath, JSON.stringify(products) , err => {
-            if(err) throw err;
-        });
+        db.execute(`UPDATE products
+                    SET name = '${data.name}', price = ${data.price}
+                    WHERE id = ${id}`);
     }
 
     static async delete(id) {
-        const products = await Product.fetchAll();
-        const newProducts = products.filter(product => product.id !== parseInt(id));
-        fs.writeFile(dataPath, JSON.stringify(newProducts) , err => {
-            if(err) throw err;
-        });
-        cartModel.delete(id);
+        db.execute(`DELETE FROM products
+                    WHERE id = ${id}`);
     }
 }
